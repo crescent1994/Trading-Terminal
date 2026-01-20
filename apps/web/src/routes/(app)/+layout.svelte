5053cbda5d2519893cca362a82ffaadac3368cab
lee <lee@g5nscghpm4.com>
2026-01-20 20:03:49 +0400

    init

---
+++ apps/web/src/routes/(app)/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import AppShell from '$lib/components/layout/AppShell.svelte';
+</script>
+
+<AppShell>
+	<slot></slot>
+</AppShell>
---
+++ apps/web/src/routes/(app)/overview/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<div class="flex justify-center gap-4 pt-10 font-bold">HOME</div>
